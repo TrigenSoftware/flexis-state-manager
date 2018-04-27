80875ada76a1c3c83e94dec52c8168d2c6af3bad
Dan Green <danon0404@gmail.com>
2018-04-28 02:34:26 +0700

    exports fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Store from './Store';
 import Actions from './Actions';
+import Provider from './Provider';
 import Connect from './Connect';
 
 export default Store;
 export {
 	Actions,
+	Provider,
 	Connect
 };
